6e12d98411119eff841e052d29df7ca719779f1f
Hiren Gohil <gohilashvinbhai324@gmail.com>
2025-07-30 12:24:55 +0000

    // For Firebase JS SDK v7.20.0 and later, measurementId is optional cons

--- src/lib/admins.ts
+++ src/lib/admins.ts
@@ -1,4 +1,13 @@
 // This is a list of user UIDs that have admin access.
-// IMPORTANT: Replace this with the actual Firebase UID of your admin user.
-// You can get this from the Firebase Authentication console.
-export const ADMIN_UIDS = ['REPLACE_WITH_ADMIN_FIREBASE_UID'];
+//
+// HOW TO ADD YOURSELF AS AN ADMIN:
+// 1. Sign up for your application with an email and password.
+// 2. Go to the Firebase Console: https://console.firebase.google.com/
+// 3. Navigate to your project -> Authentication -> Users tab.
+// 4. Find the account you just created and copy its "User UID".
+// 5. Paste the UID into the array below, replacing the placeholder.
+//
+// IMPORTANT: For security, you might want to disable sign-ups after
+// creating your admin account. You can do this in the Firebase Console
+// under Authentication -> Sign-in method.
+export const ADMIN_UIDS = ['REPLACE_WITH_YOUR_ADMIN_FIREBASE_UID'];
