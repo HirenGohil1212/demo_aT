494d51d79e96ed7438fa11259dbe9170afff04ba
Hiren Gohil <gohilashvinbhai324@gmail.com>
2025-07-31 15:44:31 +0000

    autoscroll continuesly works non stop

--- src/app/_components/home-carousel.tsx
+++ src/app/_components/home-carousel.tsx
@@ -24,7 +24,7 @@ export function HomeCarousel({ banners }: HomeCarouselProps) {
     const [api, setApi] = React.useState<CarouselApi>()
     
     const plugin = React.useRef(
-        Autoplay({ delay: 5000, stopOnInteraction: true, stopOnMouseEnter: true })
+        Autoplay({ delay: 5000, stopOnInteraction: true })
     );
 
     // This effect re-initializes the carousel when the number of banners changes.
@@ -50,7 +50,7 @@ export function HomeCarousel({ banners }: HomeCarouselProps) {
             }}
             plugins={[plugin.current]}
             onMouseEnter={plugin.current.stop}
-            onMouseLeave={plugin.current.reset}
+            onMouseLeave={plugin.current.play}
           >
             <CarouselContent>
               {banners.map((banner, index) => (
